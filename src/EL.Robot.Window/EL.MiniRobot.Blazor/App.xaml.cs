e08355ab7400ba09bc4cb3a16f65c03bd8b9e770
panyu <panyun.li@outlook.com>
2023-04-18 02:45:05 +0800

    修改window 平台

--- src/EL.Robot.Window/EL.MiniRobot.Blazor/App.xaml.cs
+++ src/EL.Robot.Window/EL.MiniRobot.Blazor/App.xaml.cs
@@ -1,12 +1,31 @@
-﻿namespace EL.MiniRobot.Blazor
+﻿using Microsoft.UI;
+using Windows.Graphics;
+
+namespace EL.MiniRobot.Blazor
 {
     public partial class App : Application
     {
+        const int WindowWidth = 950;
+        const int WindowHeight = 600;
         public App()
         {
             InitializeComponent();
 
             MainPage = new MainPage();
+            //设置窗口的大小
+            Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+            {
+#if (WINDOWS)
+                var mauiWindow = handler.VirtualView;
+                var nativeWindow = handler.PlatformView;
+                nativeWindow.Activate();
+                IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+                WindowId windowId = Win32Interop.GetWindowIdFromWindow(windowHandle);
+                Microsoft.UI.Windowing.AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+                appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight));
+#endif
+
+            });
         }
     }
 }
