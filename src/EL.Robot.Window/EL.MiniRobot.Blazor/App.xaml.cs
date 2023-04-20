5017499c7a0a0dde6c8c36499e2d3d168449d317
panyu <panyun.li@outlook.com>
2023-04-20 19:30:30 +0800

    x修改

--- src/EL.Robot.Window/EL.MiniRobot.Blazor/App.xaml.cs
+++ src/EL.Robot.Window/EL.MiniRobot.Blazor/App.xaml.cs
@@ -1,20 +1,24 @@
-﻿using Microsoft.UI;
+﻿#if (WINDOWS)
+using Microsoft.UI;
+using Microsoft.VisualBasic;
 using Windows.Graphics;
+using Colors= Microsoft.UI.Colors;
+using Microsoft.UI.Windowing;
+#endif
 
 namespace EL.MiniRobot.Blazor
 {
-    public partial class App : Application
-    {
-        const int WindowWidth = 950;
-        const int WindowHeight = 600;
-        public App()
-        {
-            InitializeComponent();
-
-            MainPage = new MainPage();
-            //设置窗口的大小
-            Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
-            {
+	public partial class App : Application
+	{
+		const int WindowWidth = 950;
+		const int WindowHeight = 600;
+		public App()
+		{
+			InitializeComponent();
+			MainPage = new MainPage();
+			//设置窗口的大小
+			Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+			{
 #if (WINDOWS)
                 var mauiWindow = handler.VirtualView;
                 var nativeWindow = handler.PlatformView;
@@ -23,9 +27,30 @@ namespace EL.MiniRobot.Blazor
                 WindowId windowId = Win32Interop.GetWindowIdFromWindow(windowHandle);
                 Microsoft.UI.Windowing.AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
                 appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight));
+				var titleBar = appWindow.TitleBar;
+            
+                //titleBar.ExtendsContentIntoTitleBar = true;
+    //               titleBar.IconShowOptions = IconShowOptions.HideIconAndSystemMenu;
+                  //titleBar.PreferredHeightOption  = TitleBarHeightOption.Tall;
+				 //titleBar.ForegroundColor =  Colors.White;
+     //           titleBar.BackgroundColor = Colors.Green;
+     //           titleBar.ButtonForegroundColor = Colors.White;
+     //           titleBar.ButtonBackgroundColor = Colors.SeaGreen;
+     //           titleBar.ButtonHoverForegroundColor = Colors.Gainsboro;
+     //           titleBar.ButtonHoverBackgroundColor = Colors.DarkSeaGreen;
+     //           titleBar.ButtonPressedForegroundColor = Colors.Gray;
+     //           titleBar.ButtonPressedBackgroundColor = Colors.LightGreen;
+
+                // Set inactive window colors
+                // Note: No effect when app is running on Windows 10 since color customization is not
+                // supported.
+                //titleBar.InactiveForegroundColor = Colors.Gainsboro;
+                //titleBar.InactiveBackgroundColor = Colors.SeaGreen;
+                //titleBar.ButtonInactiveForegroundColor = Colors.Gainsboro;
+                //titleBar.ButtonInactiveBackgroundColor = Colors.SeaGreen;
 #endif
 
             });
-        }
-    }
+		}
+	}
 }
--- src/EL.Robot.Window/EL.MiniRobot.Blazor/MainPage.xaml.cs
+++ src/EL.Robot.Window/EL.MiniRobot.Blazor/MainPage.xaml.cs
@@ -1,10 +1,62 @@
-﻿namespace EL.MiniRobot.Blazor
+﻿//#if (WINDOWS)
+//using Microsoft.UI;
+//using Microsoft.VisualBasic;
+//using Windows.Graphics;
+//using Colors= Microsoft.UI.Colors;
+//using Microsoft.UI.Windowing;
+//#endif
+//using static System.Net.Mime.MediaTypeNames;
+//using System.Xml.Linq;
+
+namespace EL.MiniRobot.Blazor
 {
-    public partial class MainPage : ContentPage
-    {
-        public MainPage()
-        {
-            InitializeComponent();
-        }
-    }
+	public partial class MainPage : ContentPage
+	{
+
+
+		public MainPage()
+		{
+			InitializeComponent();
+            this.Title = "test";
+			Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+			{
+
+				
+
+#if (WINDOWS)
+    //            var mauiWindow = handler.VirtualView;
+    //            var nativeWindow = handler.PlatformView;
+    //            nativeWindow.Activate();
+    //            IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+    //            WindowId windowId = Win32Interop.GetWindowIdFromWindow(windowHandle);
+    //            Microsoft.UI.Windowing.AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+    //            appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight));
+				//var titleBar = appWindow.TitleBar;
+           
+    //            titleBar.ExtendsContentIntoTitleBar = true;
+    //               titleBar.IconShowOptions = IconShowOptions.HideIconAndSystemMenu;
+    //               titleBar.ButtonBackgroundColor = Colors.Transparent;
+    //titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+				 //titleBar.ForegroundColor =  Colors.White;
+                //titleBar.BackgroundColor = Colors.Green;
+                //titleBar.ButtonForegroundColor = Colors.White;
+                //titleBar.ButtonBackgroundColor = Colors.SeaGreen;
+                //titleBar.ButtonHoverForegroundColor = Colors.Gainsboro;
+                //titleBar.ButtonHoverBackgroundColor = Colors.DarkSeaGreen;
+                //titleBar.ButtonPressedForegroundColor = Colors.Gray;
+                //titleBar.ButtonPressedBackgroundColor = Colors.LightGreen;
+
+                //// Set inactive window colors
+                //// Note: No effect when app is running on Windows 10 since color customization is not
+                //// supported.
+                //titleBar.InactiveForegroundColor = Colors.Gainsboro;
+                //titleBar.InactiveBackgroundColor = Colors.SeaGreen;
+                //titleBar.ButtonInactiveForegroundColor = Colors.Gainsboro;
+                //titleBar.ButtonInactiveBackgroundColor = Colors.SeaGreen;
+#endif
+
+			});
+		}
+
+	}
 }
